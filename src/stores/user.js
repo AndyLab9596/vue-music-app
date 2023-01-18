18adfcd7462671161b187208eeb69fdbc34c2ac6
andyvy <thienvy95@gmail.com>
2023-01-18 14:25:00 +0700

    Login Logout functionalities

--- src/stores/user.js
+++ src/stores/user.js
@@ -7,16 +7,38 @@ export const useUserStore = defineStore("user", {
   }),
   actions: {
     async register(values) {
-      await auth.createUserWithEmailAndPassword(values.email, values.password);
+      const userCred = await auth.createUserWithEmailAndPassword(
+        values.email,
+        values.password
+      );
 
-      await usersCollection.add({
+      // await usersCollection.add({
+      //   name: values.name,
+      //   email: values.email,
+      //   age: values.age,
+      //   country: values.country,
+      // });
+
+      await usersCollection.doc(userCred.user.uid).set({
         name: values.name,
         email: values.email,
         age: values.age,
         country: values.country,
       });
 
+      await userCred.user.updateProfile({
+        displayName: values.name,
+      });
+
       this.userLoggedIn = true;
+    },
+    async authenticate(values) {
+      await auth.signInWithEmailAndPassword(values.email, values.password);
+      this.userLoggedIn = true;
+    },
+    async signOut() {
+      await auth.signOut();
+      this.userLoggedIn = false;
     },
   },
 });
