c8abef68319c041151c14975c99d54d7af2d3d98
andyvy <thienvy95@gmail.com>
2023-01-18 16:58:14 +0700

    Changing to name route

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import ManageView from "@/views/ManageView.vue";
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: HomeView,
+  },
+  {
+    name: "about",
+    path: "/about",
+    component: AboutView,
+  },
+  {
+    name: "manage",
+    path: "/manage",
+    component: ManageView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+  linkExactActiveClass: "text-yellow-500",
+});
 
-export default router
+export default router;
