8fcff08b38c78b38c09375097ed501a7bb9b6a4a
andyvy <thienvy95@gmail.com>
2023-01-18 09:38:26 +0700

    create firestore fireauth and user store

---
+++ src/includes/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_K4NLLCXrD41xWsdtqLeOK7km94MjF28",
+  authDomain: "vue2-music.firebaseapp.com",
+  projectId: "vue2-music",
+  storageBucket: "vue2-music.appspot.com",
+  appId: "1:224430415551:web:f169ac1364ff1397a7f00a",
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection("users");
+
+export { auth, db, usersCollection };
