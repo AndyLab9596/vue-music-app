becf48cc28f9c0c02efaaa5f09ab3d41026268a3
andyvy <thienvy95@gmail.com>
2023-01-19 14:56:28 +0700

    Upload to storage and link to firestore

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -22,32 +22,17 @@
         </div>
         <hr class="my-6" />
         <!-- Progess Bars -->
-        <div class="mb-4">
+        <div class="mb-4" v-for="upload in uploads" :key="upload.name">
           <!-- File Name -->
-          <div class="font-bold text-sm">Just another song.mp3</div>
+          <div class="font-bold text-sm" :class="upload.text_class">
+            <i :class="upload.icon" class="mr-1"></i>{{ upload.name }}
+          </div>
           <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
             <!-- Inner Progress Bar -->
             <div
-              class="transition-all progress-bar bg-blue-400"
-              style="width: 75%"
-            ></div>
-          </div>
-        </div>
-        <div class="mb-4">
-          <div class="font-bold text-sm">Just another song.mp3</div>
-          <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-            <div
-              class="transition-all progress-bar bg-blue-400"
-              style="width: 35%"
-            ></div>
-          </div>
-        </div>
-        <div class="mb-4">
-          <div class="font-bold text-sm">Just another song.mp3</div>
-          <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-            <div
-              class="transition-all progress-bar bg-blue-400"
-              style="width: 55%"
+              class="transition-all progress-bar"
+              :class="upload.variant"
+              :style="{ width: upload.current_progress + '%' }"
             ></div>
           </div>
         </div>
@@ -57,13 +42,14 @@
 </template>
 
 <script>
-import { storage } from "@/includes/firebase";
+import { storage, auth, songsCollection } from "@/includes/firebase";
 
 export default {
   name: "UploadFile",
   data() {
     return {
       is_dragover: false,
+      uploads: [],
     };
   },
   methods: {
@@ -78,7 +64,50 @@ export default {
         }
         const storageRef = storage.ref(); // vue2-music.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`); // vue2-music.appspot.com/songs/example.mp3
-        songsRef.put(file);
+        const task = songsRef.put(file);
+
+        // Array.push return the new length of that array, subtract 1 to get the latest index
+        const uploadIndex =
+          this.uploads.push({
+            task,
+            current_progress: 0,
+            name: file.name,
+            variant: "bg-blue-400",
+            icon: "fas fa-spinner fa-spin",
+            text_class: "",
+          }) - 1;
+
+        task.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            this.uploads[uploadIndex].current_progress = progress;
+          },
+          (error) => {
+            this.uploads[uploadIndex].variant = "bg-red-400";
+            this.uploads[uploadIndex].icon = "fas fa-times";
+            this.uploads[uploadIndex].text_class = "text-red-400";
+            console.log(error);
+          },
+          async () => {
+            const song = {
+              uid: auth.currentUser.uid,
+              display_name: auth.currentUser.displayName,
+              original_name: task.snapshot.ref.name,
+              modified_name: task.snapshot.ref.name,
+              genre: "",
+              comment_count: 0,
+            };
+
+            song.url = await task.snapshot.ref.getDownloadURL();
+            await songsCollection.add(song);
+
+            this.uploads[uploadIndex].variant = "bg-green-400";
+            this.uploads[uploadIndex].icon = "fas fa-check";
+            this.uploads[uploadIndex].text_class = "text-green-400";
+          }
+        );
       });
     },
   },
