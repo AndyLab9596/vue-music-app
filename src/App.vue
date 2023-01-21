8d5f571c68a65eda49b22b7f4fbd1f6653879c0d
andyvy <thienvy95@gmail.com>
2023-01-21 09:21:12 +0700

    Transition with route

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div>
     <!-- Header -->
     <app-header />
-    <router-view></router-view>
+
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
+
     <app-player />
 
     <!-- Auth Modal -->
@@ -35,3 +41,18 @@ export default {
   },
 };
 </script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
